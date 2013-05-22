9ba8b585cee08272166a872fa2a9631994cc7678
Lester B <tkhdlstfl.l+git@gmail.com>
2013-05-22 09:23:50 +0800

    Fix whitespace issues

--- dropboxrunner.sh
+++ dropboxrunner.sh
@@ -3,10 +3,10 @@
 # Part of the code copied from
 # https://wiki.archlinux.org/index.php/Dropbox#Multiple_Dropbox_Instances
 
-#*******************************                                                                        
-# Multiple dropbox instances                                                                            
-#*******************************                                                                        
-                                                                                                        
+#*******************************
+# Multiple dropbox instances
+#*******************************
+
 : ${XDG_CONFIG_HOME:="$HOME/.config"}
 dropbox_mul_config=dropboxes
 dboxes_dir_config="$XDG_CONFIG_HOME/$dropbox_mul_config"
@@ -14,10 +14,10 @@ dboxes_dir_config="$XDG_CONFIG_HOME/$dropbox_mul_config"
 mkdir -p "$dboxes_dir_config"
 touch -a "$dboxes_dir_config/dbox_location"
 cat "$dboxes_dir_config/dbox_location" | while read dropbox;
-do                                                                                                      
-    if ! [ -d "$dropbox" ];then                                                                     
-        mkdir "$dropbox"                                                                            
+do
+    if ! [ -d "$dropbox" ];then
+        mkdir "$dropbox"
         ln -s "$HOME/.Xauthority" "$dropbox/.Xauthority"
-    fi                                                                                                  
+    fi
     HOME="$dropbox/" dropboxd start -i
-done  
+done
